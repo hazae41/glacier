5db10eac87ac37f244792d094687e87ac8ef955d
hazae41 <hazae41@gmail.com>
2022-08-20 00:19:36 +0200

    theme

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,14 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/hazae41/xswr',
+  docsRepositoryBase: 'https://github.com/hazae41/xswr/blob/docs',
+  titleSuffix: " – XSWR",
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">
+        xswr
+      </span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        The simplest React data (re)fetching library ever made
       </span>
     </>
   ),
@@ -16,15 +18,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="XSWR: The simplest React data (re)fetching library ever made" />
+      <meta name="og:description" content="XSWR: The simplest React data (re)fetching library ever made" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:image" content="https://xswr.hazae41.me/og.png" />
+      <meta name="twitter:site:domain" content="xswr.hazae41.me" />
+      <meta name="twitter:url" content="https://xswr.hazae41.me" />
+      <meta name="og:title" content="The simplest React data (re)fetching library ever made" />
+      <meta name="og:image" content="https://xswr.hazae41.me/og.png" />
+      <meta name="apple-mobile-web-app-title" content="XSWR" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -62,6 +64,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} © XSWR.</>,
   unstable_faviconGlyph: '👋',
 }
