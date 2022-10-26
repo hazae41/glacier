109a18234a6a8a062d37046e9d30d0ff663fb912
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2022-10-26 18:43:23 +0200

    fix return of updater

--- dist/cjs/mods/single/helper.js
+++ dist/cjs/mods/single/helper.js
@@ -3,7 +3,6 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 
 var tslib = require('tslib');
-var generator = require('../../libs/generator.js');
 var time = require('../../libs/time.js');
 var abort = require('../errors/abort.js');
 var defaults = require('../utils/defaults.js');
@@ -114,14 +113,13 @@ var SingleHelper = /** @class */ (function () {
      * @throws Error
      */
     SingleHelper.prototype.update = function (key, skey, fetcher, updater, aborter, params) {
-        var e_1, _a;
         if (aborter === void 0) { aborter = new AbortController(); }
         if (params === void 0) { params = {}; }
         return tslib.__awaiter(this, void 0, void 0, function () {
-            var _b, current, skip, _c, dcooldown, _d, dexpiration, _e, dtimeout, timeout, signal, generator$1, _loop_1, this_1, generator_1, generator_1_1, e_1_1, result, data, error_3, _f, time_2, _g, cooldown_2, _h, expiration_2, state_2, error_2;
+            var _a, current, skip, _b, dcooldown, _c, dexpiration, _d, dtimeout, timeout, signal, generator, result, _loop_1, this_1, state_2, data, error_3, _e, time_2, _f, cooldown_2, _g, expiration_2, state_3, error_2;
             var _this = this;
-            return tslib.__generator(this, function (_j) {
-                switch (_j.label) {
+            return tslib.__generator(this, function (_h) {
+                switch (_h.label) {
                     case 0:
                         if (key === undefined)
                             return [2 /*return*/];
@@ -146,27 +144,31 @@ var SingleHelper = /** @class */ (function () {
                                 });
                             }); })];
                     case 1:
-                        _b = _j.sent(), current = _b.current, skip = _b.skip;
+                        _a = _h.sent(), current = _a.current, skip = _a.skip;
                         if (skip)
                             return [2 /*return*/, current];
-                        _c = params.cooldown, dcooldown = _c === void 0 ? defaults.DEFAULT_COOLDOWN : _c, _d = params.expiration, dexpiration = _d === void 0 ? defaults.DEFAULT_EXPIRATION : _d, _e = params.timeout, dtimeout = _e === void 0 ? defaults.DEFAULT_TIMEOUT : _e;
+                        _b = params.cooldown, dcooldown = _b === void 0 ? defaults.DEFAULT_COOLDOWN : _b, _c = params.expiration, dexpiration = _c === void 0 ? defaults.DEFAULT_EXPIRATION : _c, _d = params.timeout, dtimeout = _d === void 0 ? defaults.DEFAULT_TIMEOUT : _d;
                         timeout = setTimeout(function () {
                             aborter.abort("Update timed out");
                         }, dtimeout);
-                        _j.label = 2;
+                        _h.label = 2;
                     case 2:
-                        _j.trys.push([2, 23, 26, 27]);
+                        _h.trys.push([2, 12, 15, 16]);
                         signal = aborter.signal;
-                        generator$1 = updater(current, { signal: signal });
-                        _j.label = 3;
-                    case 3:
-                        _j.trys.push([3, 9, 10, 15]);
+                        generator = updater(current, { signal: signal });
+                        result = undefined;
                         _loop_1 = function () {
-                            var _k, data_1, error_4, optimistic;
-                            return tslib.__generator(this, function (_l) {
-                                switch (_l.label) {
-                                    case 0:
-                                        _k = generator_1_1.value, data_1 = _k.data, error_4 = _k.error;
+                            var _j, done, value, data_1, error_4, optimistic;
+                            return tslib.__generator(this, function (_k) {
+                                switch (_k.label) {
+                                    case 0: return [4 /*yield*/, generator.next()];
+                                    case 1:
+                                        _j = _k.sent(), done = _j.done, value = _j.value;
+                                        if (done) {
+                                            result = value;
+                                            return [2 /*return*/, "break"];
+                                        }
+                                        data_1 = value.data, error_4 = value.error;
                                         if (signal.aborted)
                                             throw new abort.AbortError(signal);
                                         optimistic = {};
@@ -174,82 +176,61 @@ var SingleHelper = /** @class */ (function () {
                                             optimistic.data = data_1;
                                         optimistic.error = error_4;
                                         return [4 /*yield*/, this_1.core.mutate(skey, current, function (c) { return (tslib.__assign({ time: c === null || c === void 0 ? void 0 : c.time, aborter: aborter, optimistic: true }, optimistic)); }, params)];
-                                    case 1:
-                                        current = _l.sent();
+                                    case 2:
+                                        current = _k.sent();
                                         return [2 /*return*/];
                                 }
                             });
                         };
                         this_1 = this;
-                        generator_1 = tslib.__asyncValues(generator$1);
-                        _j.label = 4;
-                    case 4: return [4 /*yield*/, generator_1.next()];
-                    case 5:
-                        if (!(generator_1_1 = _j.sent(), !generator_1_1.done)) return [3 /*break*/, 8];
+                        _h.label = 3;
+                    case 3:
                         return [5 /*yield**/, _loop_1()];
-                    case 6:
-                        _j.sent();
-                        _j.label = 7;
-                    case 7: return [3 /*break*/, 4];
-                    case 8: return [3 /*break*/, 15];
-                    case 9:
-                        e_1_1 = _j.sent();
-                        e_1 = { error: e_1_1 };
-                        return [3 /*break*/, 15];
-                    case 10:
-                        _j.trys.push([10, , 13, 14]);
-                        if (!(generator_1_1 && !generator_1_1.done && (_a = generator_1.return))) return [3 /*break*/, 12];
-                        return [4 /*yield*/, _a.call(generator_1)];
-                    case 11:
-                        _j.sent();
-                        _j.label = 12;
-                    case 12: return [3 /*break*/, 14];
-                    case 13:
-                        if (e_1) throw e_1.error;
-                        return [7 /*endfinally*/];
-                    case 14: return [7 /*endfinally*/];
-                    case 15: return [4 /*yield*/, generator.returnOf(generator$1)];
-                    case 16:
-                        result = _j.sent();
-                        if (!(result === undefined)) return [3 /*break*/, 18];
+                    case 4:
+                        state_2 = _h.sent();
+                        if (state_2 === "break")
+                            return [3 /*break*/, 5];
+                        return [3 /*break*/, 3];
+                    case 5:
+                        if (!(result === undefined)) return [3 /*break*/, 7];
                         if (fetcher === undefined)
                             throw new Error("Updater returned nothing and undefined fetcher");
                         return [4 /*yield*/, fetcher(key, { signal: signal, cache: "reload" })];
-                    case 17:
-                        result = _j.sent();
-                        _j.label = 18;
-                    case 18:
-                        data = result.data, error_3 = result.error, _f = result.time, time_2 = _f === void 0 ? Date.now() : _f, _g = result.cooldown, cooldown_2 = _g === void 0 ? time.getTimeFromDelay(dcooldown) : _g, _h = result.expiration, expiration_2 = _h === void 0 ? time.getTimeFromDelay(dexpiration) : _h;
+                    case 6:
+                        result = _h.sent();
+                        _h.label = 7;
+                    case 7:
+                        data = result.data, error_3 = result.error, _e = result.time, time_2 = _e === void 0 ? Date.now() : _e, _f = result.cooldown, cooldown_2 = _f === void 0 ? time.getTimeFromDelay(dcooldown) : _f, _g = result.expiration, expiration_2 = _g === void 0 ? time.getTimeFromDelay(dexpiration) : _g;
                         if (signal.aborted)
                             throw new abort.AbortError(signal);
                         return [4 /*yield*/, this.core.get(skey, params)];
-                    case 19:
-                        current = _j.sent();
-                        if (!(error_3 !== undefined)) return [3 /*break*/, 21];
+                    case 8:
+                        current = _h.sent();
+                        if (!(error_3 !== undefined)) return [3 /*break*/, 10];
                         if ((current === null || current === void 0 ? void 0 : current.aborter) !== aborter)
                             return [2 /*return*/, current];
                         return [4 /*yield*/, this.core.mutate(skey, current, function (c) { return ({ time: c === null || c === void 0 ? void 0 : c.time, cooldown: cooldown_2, expiration: expiration_2, aborter: undefined, optimistic: false, data: c === null || c === void 0 ? void 0 : c.data, error: error_3 }); }, params)];
-                    case 20: return [2 /*return*/, _j.sent()];
-                    case 21:
-                        state_2 = {};
+                    case 9: return [2 /*return*/, _h.sent()];
+                    case 10:
+                        state_3 = {};
                         if (data !== undefined)
-                            state_2.data = data;
-                        state_2.error = error_3;
-                        return [4 /*yield*/, this.core.mutate(skey, current, function () { return (tslib.__assign({ time: time_2, cooldown: cooldown_2, expiration: expiration_2, aborter: undefined, optimistic: false }, state_2)); }, params)];
-                    case 22: return [2 /*return*/, _j.sent()];
-                    case 23:
-                        error_2 = _j.sent();
+                            state_3.data = data;
+                        state_3.error = error_3;
+                        return [4 /*yield*/, this.core.mutate(skey, current, function () { return (tslib.__assign({ time: time_2, cooldown: cooldown_2, expiration: expiration_2, aborter: undefined, optimistic: false }, state_3)); }, params)];
+                    case 11: return [2 /*return*/, _h.sent()];
+                    case 12:
+                        error_2 = _h.sent();
                         return [4 /*yield*/, this.core.get(skey, params)];
-                    case 24:
-                        current = _j.sent();
+                    case 13:
+                        current = _h.sent();
                         if ((current === null || current === void 0 ? void 0 : current.aborter) !== aborter)
                             return [2 /*return*/, current];
                         return [4 /*yield*/, this.core.mutate(skey, current, function (c) { return ({ time: c === null || c === void 0 ? void 0 : c.time, aborter: undefined, optimistic: false, data: c === null || c === void 0 ? void 0 : c.data, error: error_2 }); }, params)];
-                    case 25: return [2 /*return*/, _j.sent()];
-                    case 26:
+                    case 14: return [2 /*return*/, _h.sent()];
+                    case 15:
                         clearTimeout(timeout);
                         return [7 /*endfinally*/];
-                    case 27: return [2 /*return*/];
+                    case 16: return [2 /*return*/];
                 }
             });
         });
--- src/libs/generator.ts
+++
@@ -1,9 +0,0 @@
-export async function nextOf<T, R>(generator: AsyncGenerator<T, R>) {
-  const next = await generator.next()
-  if (!next.done) return next.value
-}
-
-export async function returnOf<T, R>(generator: AsyncGenerator<T, R>) {
-  const next = await generator.next()
-  if (next.done) return next.value
-}
