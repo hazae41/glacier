fd7c80667f29df7a0441d24c803279876019d2b9
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-01-04 01:43:52 +0100

    rollup and update

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import ts from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import externals from "rollup-plugin-node-externals";
 import typescript from "ttypescript";
 
 export const config = [
@@ -10,29 +11,30 @@ export const config = [
     output: [{
       dir: "./dist/esm",
       format: "esm",
+      exports: "named",
       preserveModules: true,
       sourcemap: true,
-      entryFileNames: "[name].mjs"
+      entryFileNames: "[name].mjs",
     }, {
       dir: "./dist/cjs",
       format: "cjs",
+      exports: "named",
       preserveModules: true,
       sourcemap: true,
       entryFileNames: "[name].cjs",
     }],
-    plugins: [resolve(), ts({ typescript }), commonjs()],
-    external: ["react", "tslib"]
+    plugins: [resolve(), externals(), ts({ typescript }), commonjs()]
   },
   {
     input: "./src/index.ts",
     output: [{
       dir: "./dist/types",
       format: "esm",
+      exports: "named",
       preserveModules: true,
       entryFileNames: "[name].d.ts",
     }],
-    plugins: [dts(), resolve(), ts({ typescript })],
-    external: ["react", "tslib"]
+    plugins: [dts(), resolve(), externals(), ts({ typescript })]
   },
   {
     input: "./src/index.test.ts",
@@ -44,8 +46,7 @@ export const config = [
       sourcemap: true,
       entryFileNames: "[name].cjs",
     }],
-    plugins: [resolve(), ts({ typescript }), commonjs()],
-    external: ["react", "tslib", "@hazae41/phobos"]
+    plugins: [resolve(), externals(), ts({ typescript }), commonjs()]
   },
 ]
 
