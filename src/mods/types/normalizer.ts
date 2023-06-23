f1dcab00e38b47959ca2f5264e98bb1369fec60d
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-06-23 12:11:17 +0200

    fix

--- src/mods/types/normalizer.ts
+++ src/mods/types/normalizer.ts
@@ -3,7 +3,7 @@ import { Core } from "mods/core/core.js"
 import { Fetched } from "mods/result/fetched.js"
 
 export type Normalizer<D, F> =
-  (fetched: Optional<Fetched<D, F>>, more: NormalizerMore) => Promise<Fetched<D, F>>
+  (fetched: Optional<Fetched<D, F>>, more: NormalizerMore) => Promise<Optional<Fetched<D, F>>>
 
 export interface NormalizerMore {
   readonly core: Core,
