30e344a96fe230f5515bc395220f8b101a36958f
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-07-09 16:18:54 +0200

    fix

--- src/mods/types/index.ts
+++ src/mods/types/index.ts
@@ -1,8 +1,8 @@
 export * from "./fetcher.js"
+export * from "./indexer.js"
 export * from "./mutator.js"
 export * from "./normalizer.js"
 export * from "./scroller.js"
 export * from "./settings.js"
 export * from "./state.js"
 export * from "./updater.js"
-
