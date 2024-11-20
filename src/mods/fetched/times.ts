a908b7674c5818844b7bfde6907594b69b6c36df
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-11-20 23:48:36 +0700

    d

--- src/mods/fetched/times.ts
+++ src/mods/fetched/times.ts
@@ -1,26 +1,26 @@
 import { Nullable } from "@hazae41/option"
 import { Time } from "libs/time/time.js"
 
-export interface TimesInit {
-  readonly time?: Nullable<number>,
-  readonly cooldown?: Nullable<number>,
-  readonly expiration?: Nullable<number>
-}
-
 export interface Times {
   readonly time: number,
   readonly cooldown?: Nullable<number>,
   readonly expiration?: Nullable<number>
 }
 
-export interface CooldownAndExpiration {
+export interface TimesInit {
+  readonly time?: Nullable<number>,
+  readonly cooldown?: Nullable<number>,
+  readonly expiration?: Nullable<number>
+}
+
+export interface DelaysInit {
   readonly cooldown?: Nullable<number>,
   readonly expiration?: Nullable<number>
 }
 
 export namespace TimesInit {
 
-  export function merge(times: TimesInit, delays?: CooldownAndExpiration): TimesInit {
+  export function merge(times: TimesInit, delays?: DelaysInit): TimesInit {
     const {
       time,
       cooldown = Time.fromDelay(delays?.cooldown),
