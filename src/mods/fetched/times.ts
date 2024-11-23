67ee1c1be7e01216126982a191cba05fedef481e
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-11-23 13:12:20 +0700

    times min

--- src/mods/fetched/times.ts
+++ src/mods/fetched/times.ts
@@ -15,3 +15,19 @@ export interface CachedInit {
   readonly cooldown?: number
   readonly expiration?: number
 }
+
+export namespace Times {
+
+  export function min(...times: (Timed & Cached)[]): Timed & Cached {
+    const time = Math.min(...times.map(t => t.time))
+
+    const cooldownOrInfinity = Math.min(...times.map(t => t.cooldown || Infinity))
+    const expirationOrInfinity = Math.min(...times.map(t => t.expiration || Infinity))
+
+    const cooldown = cooldownOrInfinity === Infinity ? undefined : cooldownOrInfinity
+    const expiration = expirationOrInfinity === Infinity ? undefined : expirationOrInfinity
+
+    return { time, cooldown, expiration }
+  }
+
+}
